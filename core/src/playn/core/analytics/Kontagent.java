2c6b1e128ff92b61cf3cfba41cd27ef0b6f52a99
Dustin McCartney <dmccartney@altego.com>
2012-09-19 17:12:52 -0700

    changed UID to String

--- core/src/playn/core/analytics/Kontagent.java
+++ core/src/playn/core/analytics/Kontagent.java
@@ -4,6 +4,10 @@
  */
 package playn.core.analytics;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import playn.core.Analytics;
 import playn.core.PlayN;
 import playn.core.util.Callback;
@@ -14,14 +18,14 @@ import playn.core.util.Callback;
  */
 public class Kontagent implements Analytics {
     private final String apiKey;
-    private int userID;
+    private String userID;
     
-    public Kontagent(String apiKeyInput, int userIDInput)
+    public Kontagent(String apiKeyInput, String userIDInput)
     {
         this.apiKey = apiKeyInput;
         this.userID = userIDInput;
     }
-
+    
     @Override
     public void logEvent(Category category, String action) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -30,7 +34,7 @@ public class Kontagent implements Analytics {
     @Override
     public void logEvent(Category category, String action, String label, int value) {
         long timestamp = System.currentTimeMillis()/1000L;
-        String kontagentURLString = "http://api.geo.kontagent.net/api/v1/"+ this.apiKey + "/" + category.getCategory().toString() + "/?s=" + this.userID + "&n=" + label;
+        String kontagentURLString = "http://api.geo.kontagent.net/api/v1/"+ this.apiKey + "/" + category.getCategory().toString() + "/?s=" + this.userID + "&n=" + label + "&ts=" + timestamp;
         
         PlayN.net().get(kontagentURLString, null, 
             new Callback<String>(){
